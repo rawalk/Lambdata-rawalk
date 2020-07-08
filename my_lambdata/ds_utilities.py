43a9cf1095b6daac98a429fb7a688c68de78ca77
Kush Rawal <rawalk@users.noreply.github.com>
2020-07-07 20:14:48 -0700

    Second Round of Info

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -2,5 +2,6 @@ def enlarge(n):
     ''' This function will multiple input by 100 '''
     return n * 100
 
-y = int(input("choose a number: "))
-print(y, enlarge(y))
+if __name__ == '__main__':
+    y = int(input("choose a number: "))
+    print(y, enlarge(y))
---
+++ my_lambdata/test_function.py
@@ -0,0 +1,4 @@
+from my_lambdata.ds_utilities import enlarge
+
+y = 3000
+print(y, enlarge(y))
