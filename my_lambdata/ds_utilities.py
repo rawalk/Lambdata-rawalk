5dec264314a3dd192a5bc0ee201ce6acc42bf6e7
Kush Rawal <rawalk@users.noreply.github.com>
2020-07-09 19:18:03 -0700

    class notes 1

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -1,18 +1,32 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_wine
+from pdb import set_trace as breakpoint 
+
 def enlarge(n):
     ''' This function will multiple input by 100 '''
     return n * 100
 
-def test_train_val_split(X):
-    ''' This function will do test, train and validation split '''
-  X_train, X_test, y_train, y_test = train_test_split(X, test_size=0.2, random_state=1)
-  X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1) # 0.25 x 0.8 = 0.2
-  return X_train, X_val, X_test, y_train, y_val, y_test
+def train_validation_test_split(X, y, train_size=0.7, val_size=0.1, test_size=0.2, random_state=None, shuffle=True):
+            
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+    
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train_val, y_train_val, test_size=val_size/(train_size+val_size), 
+        random_state=random_state, shuffle=shuffle)
+    
+    return X_train, X_val, X_test, y_train, y_val, y_test
 
 def len_counter(n):
     return len(n)
 
 if __name__ == '__main__':
-    y = int(input("choose a number: "))
-    print(y, enlarge(y))
+    # y = int(input("choose a number: "))
+    # print(y, enlarge(y))
+    raw_data = load_wine()
+    df = pd.DataFrame(data=raw_data['data'], columns=raw_data['feature_names'])
+    df['target'] = raw_data['target']
+    breakpoint()
+    print(df.shape)
+    
